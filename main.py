e46e8131d1c5495b020e08739bf4be88fbb38271
ar23aft <160408704+ar23aft@users.noreply.github.com>
2026-03-11 21:03:08 +0500

    Create main.py for project initialization
    Initiation of project setup of main file

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Healthcare Data Analytics Project.
+Computer Science Project
+By: Abdul Rafay Ahsan
+"""
+
+def main():
+    print("Healthcare Data Analytics Project Initialized")
+
+if __name__ == "__main__":
+    main()
